6bfe38e06b7497bb02be8959cd4d6c00ea349a85
Andy Mao <ymao28@jh.edu>
2021-09-01 09:05:30 -0400

    modify the script for terminal

--- day2-lunch/pie.py
+++ day2-lunch/pie.py
@@ -1,4 +1,8 @@
 import sys 
+
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+
 fs = open(sys.argv[1], "r")
 fly_dictionary = dict()
 for line in fs:
@@ -11,11 +15,11 @@ for features in list(table):
     feature_r = features.split()
     if feature_r[8] in fly_dictionary.keys():
         feature_r[8] = fly_dictionary[feature_r[8]]
-    elif:
-        feature_r[8] = sys.argv[3]
-    elif:
-        feature_r[8] = sys.argv[3]
-    print(feature_r)
+    else:
+        if len(sys.argv) == 4:
+            feature_r[8] = sys.argv[3]
+        else:
+            feature_r[8] = "N/A"
     data.append(feature_r)
 
 data[0][8] = "Uniport ID"
