9a2c77e081db37f704ec34e4d48679b0146e92dc
Andy Mao <ymao28@jh.edu>
2021-09-01 19:12:22 -0400

    annotate the script and correct previous errors per README instructions

--- day2-lunch/pie.py
+++ day2-lunch/pie.py
@@ -1,28 +1,27 @@
-import sys 
+import sys # import system argument
 
-arg1 = sys.argv[1]
-arg2 = sys.argv[2]
+#arg1 = sys.argv[1] # import system argument 1
+#arg2 = sys.argv[2] # import system argument 2
 
-fs = open(sys.argv[1], "r")
-fly_dictionary = dict()
-for line in fs:
-    line_d = line.split()
-    fly_dictionary[line_d[0]] = line_d[1]
+fs = open(sys.argv[1], "r") # open a file
+fly_dictionary = dict() # create an empty dictionary 
+for line in fs: # loop through each line
+    line_d = line.split() # split each line by space
+    fly_dictionary[line_d[0]] = line_d[1].strip("/n") # store the gene ID as keys and the protein ID as values 
 
-table = open(sys.argv[2] , 'r')
-data = []
-for features in list(table):
-    feature_r = features.split()
-    if feature_r[8] in fly_dictionary.keys():
-        feature_r[8] = fly_dictionary[feature_r[8]]
+table = open(sys.argv[2] , 'r') # open the second file
+data = [] # create an empty list 
+for features in table: # loop through each line
+    feature_r = features.split() # split by space
+    if feature_r[8] in fly_dictionary.keys(): # look for a match between the gene ID in target file and query file 
+        feature_r[8] = fly_dictionary[feature_r[8]] # if there is a match, substitute the gene ID with the Uniport ID
+    elif len(sys.argv) == 4: # If there is a third argument, substitute the gene ID with the third argument (string)
+        feature_r[8] = sys.argv[3]
     else:
-        if len(sys.argv) == 4:
-            feature_r[8] = sys.argv[3]
-        else:
-            feature_r[8] = "N/A"
-    data.append(feature_r)
+        feature_r[8] = "N/A" # If a third argument doesn't exist, put "N/A"
+    data.append(feature_r) # Collect this new list
 
-data[0][8] = "Uniport ID"
+data[0][8] = "Uniport ID" # Change the name of the header
 
-for gg in data:
+for gg in data: # join each element of the list with Tab to align the data better for visualization
     print("\t".join(gg))
